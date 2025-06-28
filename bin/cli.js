db3dde82b9fba632f2f683dd09cd5e25d34b1d29
Kasper Eklander <kasper@eklander.dk>
2025-06-28 12:15:00 +0200

    refactor: migrate CLI and tests to ES module syntax

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const packageJson = require('../package.json');
+import { program } from 'commander';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const packageJson = JSON.parse(readFileSync(path.join(__dirname, '../package.json'), 'utf8'));
 
 program
   .name('fetch-client-generator')
--- test/cli.test.js
+++ test/cli.test.js
@@ -1,6 +1,10 @@
-const { expect } = require('chai');
-const { exec } = require('child_process');
-const path = require('path');
+import { expect } from 'chai';
+import { exec } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 describe('CLI', () => {
   it('should show help when no arguments provided', (done) => {
